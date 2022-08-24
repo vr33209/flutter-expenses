be524a30546f69b866cc63063a6daaf4e08e2821
Victor Rodrigues <vr33209@gmail.com>
2022-08-23 21:19:22 -0300

    feature: add model transation and create component card

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:intl/src/intl/date_format.dart';
 
 main() => runApp(const ExpenseApp());
 
@@ -7,15 +10,80 @@ class ExpenseApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: MyHomePage());
+    return MaterialApp(home: MyHomePage());
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+  MyHomePage({super.key});
+
+  final _transactions = [
+    Transaction(
+        id: 't1', title: 'Novo Tenis', value: 310.76, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'Conta de luz', value: 211.30, date: DateTime.now()),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Versão inicial'));
+    return Scaffold(
+      appBar: AppBar(title: Text('Despesas Pessoais')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+              child: Card(
+                  child: Text('Gráfico'),
+                  elevation: 5,
+                  color: Colors.blueAccent)),
+          Column(
+              children: _transactions
+                  .map((transaction) => Card(
+                          child: Row(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.symmetric(
+                                horizontal: 15, vertical: 10),
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.purple,
+                                width: 2,
+                              ),
+                            ),
+                            padding: EdgeInsets.all(10),
+                            child: Text(
+                              '${NumberFormat.simpleCurrency(locale: 'pt_BR').format(transaction.value)}',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                color: Colors.purple,
+                              ),
+                            ),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                transaction.title,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                ),
+                              ),
+                              Text(
+                                  DateFormat('dd MMM yyy')
+                                      .format(transaction.date),
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                  )),
+                            ],
+                          )
+                        ],
+                      )))
+                  .toList())
+        ],
+      ),
+    );
   }
 }
